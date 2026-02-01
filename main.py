10c146badb97e70b41be7f0a13cceeae2b646bdb
Ds <devteachpl@gmail.com>
2026-02-01 21:08:50 +0100

    Uszybcenie ruletki

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ B - Czarny
                     if (kolor in ["R", "G", "B"]):
                         print("Losuje...")
                         trafione = random.randint(0, 36)
-                        time.sleep(10)
+                        time.sleep(1.5)
                         if (
                             kolor == "R" and trafione in [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36] or
                             kolor == "B" and trafione in [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 29, 28, 31, 33, 35]
@@ -171,7 +171,7 @@ B - Czarny
                     pole = int(input("Pole: "))
                     print("Losuje..")
                     trafione = random.randint(0, 36)
-                    time.sleep(10)
+                    time.sleep(1.5)
                     if (pole == trafione):
                         wygrane = 250 * 36
                         print(f"Udalo ci sie!! Wygrywasz {wygrane}zł")
