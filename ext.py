355282495893e65454971a0aee0881ea3c7a31a1
Ds <devteachpl@gmail.com>
2026-02-05 00:46:18 +0100

    dodanie pliku ext.py

---
+++ ext.py
@@ -0,0 +1,24 @@
+class Wiadomosci:
+    podatki = """
+Podatki zabierają ci 10% twojego stanu bankowego co 30 transakcji. 
+Sprawdzenie informacji o podatkach NIE liczy się jako transakcja.
+W momentu niskiego stanu konta, państwo nałoży dług w ilości 1000 złotych.
+"""
+    wygrana = """
+[JESZCZE NIE DA SIE WYGRAC]
+Aby wygrac musisz:
+- Mieć milion złotych
+- Wygrać raz jackpota
+- Nie mieć długów
+- Wygrać w ruletce 10 razy (kolory)
+- Wygrać raz w ruletce (pola)
+- Wygrać 25 razy w blackjacku
+- Wyjść 3 razy z lochów
+- Wygrać raz w lotto (minimum 3 liczby)
+- Mieć minimum 100 siły i zręczności w sztukach walk
+- Pracować minimum 50 razy
+- Okradnąć okradającego pracodawcę 5 razy
+I to tyle na razie
+"""
+
+# Jesli nie dziala import, przenies Wiadomosci do glownego pliku.
